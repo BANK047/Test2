b450acc8be437a6c6eb6feffb89a7b945fb4cb4d
BANK047 <bankchoojongkol@gmail.com>
2025-08-28 12:20:23 +0700

    Add files via upload

---
+++ 3_1.js
@@ -0,0 +1,18 @@
+let points = 49;
+
+if (points >= 80 && points <= 100){
+    console.log("Grade: A");
+}
+if (points >= 70 && points <= 79){
+    console.log("Grade: B");       
+}     
+if (points >= 60 && points <= 69){
+    console.log("Grade: C");       
+}     
+if (points >= 50 && points <= 59){
+    console.log("Grade: D");       
+}     
+if (points < 50 ){
+    console.log("Grade: F");       
+}     
+
---
+++ 3_2.js
@@ -0,0 +1,10 @@
+let Product_Price = 200;
+let Discount_Percentage = 0.1;
+
+pre_special_price = Product_Price*Discount_Percentage;
+real_special_price = Product_Price-pre_special_price;
+
+// if(Product_Price <= 0){
+//     console.log()
+// }
+console.log(real_special_price);
